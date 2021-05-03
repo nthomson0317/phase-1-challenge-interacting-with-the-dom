182be3e22c56826756d273ae65a62559673fb5b6
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-05-03 10:58:43 -0400

    set Increment Counter

--- js/challenge.js
+++ js/challenge.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+
     //grab the necessary objects by their IDs
     const counter = document.getElementById('counter');
     const minus = document.getElementById('minus');
@@ -6,83 +6,115 @@ document.addEventListener("DOMContentLoaded", function(event) {
     const pause = document.getElementById('pause');
     const heart = document.getElementById('heart');
     const likes = document.getElementsByClassName('likes')[0];
-    const comments = document.getElementById("comment-form")
+    const comment_form = document.getElementById("comment-form")
+    const comments = document.querySelector('#list')
+    let pasued = false
+    let numberTracker = {}
 
-    //Convert the string that was in that counter to a number type
-    let number = parseInt(counter.innerHTML,10)
+  //add event listeners on relevant objects
+
+    let interval = setInterval(incrementCounter,1000)
+    plus.addEventListener("click", incrementCounter)
+    minus.addEventListener("click", decrementCounter)
+    pause.addEventListener("click", togglePaused)
+    heart.addEventListener("click", addLike)
+    comment_form.addEventListener("submit", handleSubmit)
+
+//INCREMENT COUNTER
+function incrementCounter(event) {
+    counter.innerText = parseInt(counter.innerText) + 1
+
+}
 
 
-    //Adds event listeners that decrement/increment number
-    minus.addEventListener('click', ()=>{
-        number--;
-    })
-    plus.addEventListener('click', ()=>{
-        number++;
-    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     //Convert the string that was in that counter to a number type
+//     let number = parseInt(counter.innerHTML,10)
+
+
+//     //Adds event listeners that decrement/increment number
+//     minus.addEventListener('click', ()=>{
+//         number--;
+//     })
+//     plus.addEventListener('click', ()=>{
+//         number++;
+//     })
 
     
     
-    //sets the status of whether or not the pause button was pressed
-    let paused = false;
-    pause.addEventListener('click', ()=>{
-        if (paused){
-            paused = false;
-        }else{
-            paused = true;
-        }
-        // console.log(paused)
-    })
+//     //sets the status of whether or not the pause button was pressed
+//     let paused = false;
+//     pause.addEventListener('click', ()=>{
+//         if (paused){
+//             paused = false;
+//         }else{
+//             paused = true;
+//         }
+//         // console.log(paused)
+//     })
     
     
-    //apparently setInterval has to be called on window, the first argument 
-    //is a function that plays in a certain interval in milliseconds 
-    //which is the second argument
+//     //apparently setInterval has to be called on window, the first argument 
+//     //is a function that plays in a certain interval in milliseconds 
+//     //which is the second argument
     
-    //every 1 second, increment number and change the current value of the 
-    //counter
-    window.setInterval(() => {
-        //if it's NOT paused, then it'll increment
-        if (!paused){
-            pause.innerText = 'pause'
-            plus.disabled = false; 
-            minus.disabled = false;
-            heart.disabled = false;
-            number++
-        } else{
-            plus.disabled = true; //if it IS paused, disable the other buttons
-            minus.disabled = true;
-            heart.disabled = true;
-            pause.innerText = 'resume'
-        }
-            counter.innerHTML = number
-    },  1000);
+//     //every 1 second, increment number and change the current value of the 
+//     //counter
+//     window.setInterval(() => {
+//         //if it's NOT paused, then it'll increment
+//         if (!paused){
+//             pause.innerText = 'pause'
+//             plus.disabled = false; 
+//             minus.disabled = false;
+//             heart.disabled = false;
+//             number++
+//         } else{
+//             plus.disabled = true; //if it IS paused, disable the other buttons
+//             minus.disabled = true;
+//             heart.disabled = true;
+//             pause.innerText = 'resume'
+//         }
+//             counter.innerHTML = number
+//     },  1000);
 
-    //adds a listener to heart/ adds an li of the number the heart was pressed on
-    heart.addEventListener('click',()=>{
-        let likeCount = document.createElement('li');
-        likeCount.innerText = number;
-        console.log(likeCount.innerText);
-        likes.appendChild(likeCount)
-    })
-    //maybe have another toggle here where it's outside the event listener
-    //and appends the 'x has been clicked y times' sentence
+//     //adds a listener to heart/ adds an li of the number the heart was pressed on
+//     heart.addEventListener('click',()=>{
+//         let likeCount = document.createElement('li');
+//         likeCount.innerText = number;
+//         console.log(likeCount.innerText);
+//         likes.appendChild(likeCount)
+//     })
+//     //maybe have another toggle here where it's outside the event listener
+//     //and appends the 'x has been clicked y times' sentence
     
     
-    let ul = document.createElement('ul');
-    comments.appendChild(ul)
+//     let ul = document.createElement('ul');
+//     comments.appendChild(ul)
 
-    let ulNode = document.getElementsByTagName('ul')
-    comments.addEventListener('submit',(event)=>{
-        event.preventDefault();
-        addTask(event.target['comment-input'].value)
+//     let ulNode = document.getElementsByTagName('ul')
+//     comments.addEventListener('submit',(event)=>{
+//         event.preventDefault();
+//         addTask(event.target['comment-input'].value)
 
-        function addTask(task){
-          let pinnedText = document.createElement('li')
-          pinnedText.innerText = task
-          ulNode.appendChild(pinnedText)
-        }
-    })
-});
+//         function addTask(task){
+//           let pinnedText = document.createElement('li')
+//           pinnedText.innerText = task
+//           ulNode.appendChild(pinnedText)
+//         }
+//     })
+// });
 
 
 
